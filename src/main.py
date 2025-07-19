6d895b9bcf2d13b0ae6347938b259267612f5452
Nitin soni <90346851+nitin-soni7566@users.noreply.github.com>
2025-07-19 10:30:08 +0530

    allow cors origin for local host 3000 and 5000

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from src.database import models
+from src.database.connect import engine
 from src.routers import wheel_sepecification
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.include_router(wheel_sepecification.router)
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5000",
+]
 
-from src.database import models
-from src.database.connect import engine
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+
 models.Base.metadata.create_all(bind=engine)
+
+app.include_router(wheel_sepecification.router)
